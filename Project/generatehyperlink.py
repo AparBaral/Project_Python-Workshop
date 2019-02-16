40c3e91bf947f0486d596b1130ffcd705974205b
AparBaral <46831345+AparBaral@users.noreply.github.com>
2019-02-16 09:48:03 +0545

    Add files via upload

---
+++ Project/generatehyperlink.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+f = open('GeratedLink.txt', 'w')
+requestObj = requests.get('https://techlekh.com/')
+soup = BeautifulSoup(requestObj.text, 'html.parser')
+
+anchortags = soup.find_all('a', href=True)
+for a in anchortags:
+    if a['href'] == '#':
+        print('')
+    else:
+        f.write(str(a['href']) + '\n')
